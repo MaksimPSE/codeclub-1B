e74554bce962b91a27d647bc8046210a84457d0d
MaksimPSE <maksim@cloudflare.com>
2022-11-10 15:58:14 +0100

    First Workers homework. Return a JSON response for a POST request

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 export default {
-	fetch() {
-		return new Response('Hello worker!', {
+	fetch(request) {
+		if (request.method.toUpperCase() === 'POST') {
+		return new Response('It is a POST request, return JSON', {
 			headers: {
-				'content-type': 'text/plain',
+				'content-type': 'application/json',
 			},
 		});
+	}else{
+		return new Response('Sorry, it is NOT a POST request ' , { 
+			headers: {
+			'content-type': 'text/plain',
+		  },
+		}); 
+	}
 	},
 };
